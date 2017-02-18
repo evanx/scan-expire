e8378318dd8d0b482208d8edf47ddfdb63fb2054
Evan Summers <evan.summers@gmail.com>
2017-02-18 14:14:08 +0000

    initial

--- development/rebuild.sh
+++ development/rebuild.sh
@@ -1,7 +1,17 @@
 
-  docker build -t reo https://github.com/evanx/reo.git
-  if [ -n "$DHUSER" ]
-  then
-    docker tag reo $DHUSER/reo
-    docker push $DHUSER/reo
-  fi
+set -u -e
+
+name=`cat package.json |
+  sed 's/.*"\([0-9].*\)",/\1/'`
+
+user=`npm whoami`
+
+echo $user/$name
+
+set -x
+
+cd development
+
+  docker build -t name https://github.com/evanx/$name.git
+  docker tag $name $user/$name
+  docker push $user/$name
